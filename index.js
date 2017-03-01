e929c73d705791421f2de6d7ba7c363bf3238517
lichun <lichun@500wan.com>
2017-03-01 10:01:31 +0800

    fix bug

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function forward({req, resp, host, ip, port, path,showLog,isHttps}) {
             chunks.push(chunk)
         }).on('end', function () {
             let data = Buffer.concat(chunks);
-            showLog&&console.log(`\u001b[31m转发：\u001b[39m \u001b[36m${(endPath || req.url)}\u001b[39m 到：\u001b[32m${(host || req.headers['host'])}\u001b[39m  ip:\u001b[35m${ip}:${(port || (isHttps?443:80))}\u001b[39m \u001b[37m${resp.statusCode}\u001b[39m`);
+            showLog&&console.log(`\u001b[31m转发：\u001b[39m \u001b[36m${(path || req.url)}\u001b[39m 到：\u001b[32m${(host || req.headers['host'])}\u001b[39m  ip:\u001b[35m${ip}:${(port || (isHttps?443:80))}\u001b[39m \u001b[37m${resp.statusCode}\u001b[39m`);
             resp.writeHead(response.statusCode, response.headers);
             resp.end(data);
         })
@@ -30,7 +30,7 @@ function forward({req, resp, host, ip, port, path,showLog,isHttps}) {
             chunks.push(data);
         }).on('end', ()=>{
             let data = Buffer.concat(chunks);
-            showLog&&console.log(`\u001b[36mPOST到\u001b[36m${(endPath || req.url)}\u001b[39m--->${data}\u001b[39m`);
+            showLog&&console.log(`\u001b[36mPOST到\u001b[36m${(path || req.url)}\u001b[39m--->${data}\u001b[39m`);
             clientRequest.end(data);
         })
     }else {
